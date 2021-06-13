fc75572248ea6db5088aa0614dfa748d0089abe3
DoWhatYouL0ve <uladzimirpryshchapionak@protonmail.com>
2021-06-13 13:25:51 +0200

    fixed instance request

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-    headers: {"API-KEY": '2f9a08bc-9e7c-44f4-8d09-e14f8bc25ede'}
+    headers: {"API-KEY": '4ecc4fdb-da6b-45f9-bb99-93bccea55cd4'}
 })
 
 export const usersAPI = {
@@ -11,14 +11,10 @@ export const usersAPI = {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
     },
     follow(userId: number) {
-        return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, {},
-            {withCredentials: true, headers: {"API-KEY":'4ecc4fdb-da6b-45f9-bb99-93bccea55cd4'}})
+        return instance.post(`follow/${userId}`)
     },
     unfollow(userId: number) {
-        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, {
-            withCredentials: true,
-            headers: {"API-KEY":'4ecc4fdb-da6b-45f9-bb99-93bccea55cd4'}
-        })
+        return instance.delete(`follow/${userId}`)
     }
 }
 
